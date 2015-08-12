f8673d6602fe5fc115eb4f5ea6d9ea3ca03fa674
Kristen Spencer <kristen@kristenspencer.ca>
2015-08-12 12:25:42 -0400

    added some words

--- sidebar.php
+++ sidebar.php
@@ -2,6 +2,7 @@
 	<!-- Sidebar -->
 	<h2>Have you seen this dog?</h2>
 	<img src="<?php bloginfo('template_url')?>/images/sweetPooch.jpg" alt="" class="helloPooch">
+	<p>such files. much developers. so collaborate. wow.</p>
 
 
 	<?php dynamic_sidebar('primary-widget-area'); ?>
